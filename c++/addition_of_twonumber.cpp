616cd94a7cf2ae91686eca6f5548d7591744e3c2
Mohammed Ismail <86010610+IsmailKhan786@users.noreply.github.com>
2021-08-21 02:44:48 -0700

    Create addition_of_twonumber.cpp

---
+++ c++/addition_of_twonumber.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    cout << "Enter the Number 1:"<<endl;
+    cin>>x;
+    cout<<"Enter the Number 2:"<<endl;
+    cin>>y;
+    int z= x + y;
+    cout<<"The sum of two number is" <<z;
+
+}
